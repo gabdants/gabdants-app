81a30dea8db548f9dadcde4aa6f1e70e3f9ffbc1
Gabriel Dantas <gdantas@santander.com.br>
2020-12-19 01:14:50 -0300

    ADDED: Added pull policy rule to k8s deployment

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hi Remssa, please hire me!"
+    return "Hi Remessa, please hire me!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
